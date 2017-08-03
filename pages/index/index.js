b5d692ff28a6a1c2f897ef390220475a81b5bb86
yanyuyao <915981876@qq.com>
2017-08-04 00:22:31 +0800

    测试接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,8 @@ Page({
     autoplay: true,
     interval: 4000,
     duration: 1000,
+    userInfo: getApp().globalData.userInfo,
+    userTest: getApp().globalData.userTest,
   },
   changeIndicatorDots: function (e) {
     this.setData({
@@ -24,5 +26,32 @@ Page({
     this.setData({
       duration: e.detail.value
     })
+  },
+  createUser: function(e){
+    var l = 'https://wx.cfweb2015.com/index.php/User/Register/register/';
+    wx.request({
+      url: l,
+      data: {
+        "openId":'1234567890',
+        "avatar":"http://www.henkuai.com/uc_server/images/noavatar_middle.gif",
+        "user_nicename":'ran',
+        "sex":1,
+        "province":'山东',
+        "city":'德州',
+        "area":'禹城'
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT    
+      // header: {}, // 设置请求的 header    
+      success: function (res) {
+        var obj = {};
+        obj.userid = res.data.userid;
+        obj.nickname = res.data.nickname;
+        console.log('aaaaaaaaaaaaaaaaaaa');
+        console.log(res);
+        //obj.expires_in = Date.now() + res.data.expires_in;
+        //console.log(obj);  
+        wx.setStorageSync('user', obj);//存储openid    
+      }
+    });
   }
 })
