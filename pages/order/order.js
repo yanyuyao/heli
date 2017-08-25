73c4843d838e544273b55354204998ef83646635
yinxinxin <1278878921@qq.com>
2017-08-25 18:40:30 +0800

    order图片

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,4 +1,5 @@
 var util = require('../../utils/util.js');  
+var app = getApp();
 Page({
 
   /**
@@ -62,64 +63,14 @@ Page({
     util.userServiceStatus(this, e.currentTarget.dataset.serveid);
   },
 
-  bodytap:function(){
-    console.log(this.data.footerdisplay);
-    if(this.data.footerdisplay == 'block'){
-      this.setData({
-        footerdisplay:"none"
-      })
-    }
+  /**close点击*/
+  closetap: function (cc) {
+    this.setData({
+      footerdisplay: "none"
+    })
   },
   
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
+ 
 })
 
--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -1,4 +1,4 @@
-var utilMd5 = require('../../utils/md5.js');  
+var utilMd5 = require('../../utils/md5.js');
 var util = require('../../utils/util.js');  
 Page({
 
@@ -401,54 +401,5 @@ Page({
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    this.audioCtx = wx.createAudioContext("myAudio");
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
 
